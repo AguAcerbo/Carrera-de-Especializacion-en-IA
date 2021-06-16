a9749b3dd7c216133800ec5aca256b7f29213ef8
Agustin Acerbo <80132586+AguAcerbo@users.noreply.github.com>
2021-06-16 18:09:37 -0300

    Logistic_regretion

--- B2-Artificial_Inteligence/Repositorio_Evaluacion/logistic_regresion.py
+++ B2-Artificial_Inteligence/Repositorio_Evaluacion/logistic_regresion.py
@@ -1,10 +1,11 @@
 import numpy as np
 
 
-class LogisticRegression(object):
+class LogisticRegression(bias=False):
 
     def __init__(self):
         self.model = None
+        self.bias = False
         self.predicted = None
         self.losses = None
 
@@ -17,23 +18,30 @@ class LogisticRegression(object):
         loss = np.mean(-y * (np.log(y_hat)) - (1 - y) * np.log(1 - y_hat))
         return loss
 
-    def fit(self, x, y, lr, b, epochs, bias=True, log=100, verbose=True):
+    def fit(self, x, y, lr, b, epochs, log=100, verbose=True):
+
+        # x = dataset
+        # y = salida real
+        # lr = learning rate
+        # b = cantidad de batches
+
 
         # si decidimos utilizar bias, agregamos como siempre una columna con '1' al dataset de entrada
-        if bias:
+        if self.bias:
             x = np.hstack((np.ones((x.shape[0], 1)), x))
 
         # inicializamos aleatoriamente los pesos
-        m = x.shape[1]
+        m = x.shape[1]                                  # tendremos un peso por features
         w = np.random.randn(m).reshape(m, 1)
 
         loss_list = []
 
         # corremos Mini-Batch para optimizar los parámetros
-        for j in range(epochs):
+        for epoch in range(epochs):
             idx = np.random.permutation(x.shape[0])
             x_train = x[idx]
             y_train = y[idx]
+
             batch_size = int(len(x_train) / b)
 
             for i in range(0, len(x_train), batch_size):
@@ -44,7 +52,7 @@ class LogisticRegression(object):
                 prediction = self.sigmoid(np.sum(np.transpose(w) * batch_x, axis=1))
                 error = prediction.reshape(-1, 1) - batch_y.reshape(-1, 1)
                 grad_sum = np.sum(error * batch_x, axis=0)
-                grad_mul = 1 / batch_size * grad_sum
+                grad_mul = -2 / batch_size * grad_sum
                 gradient = np.transpose(grad_mul).reshape(-1, 1)
 
                 w = w - (lr * gradient)
@@ -52,14 +60,16 @@ class LogisticRegression(object):
             l_epoch = self.loss(y_train, self.sigmoid(np.dot(x_train, w)))
             loss_list.append(l_epoch)
             if verbose:
-                if j % log == 0:
-                    print("Epoch: {}, Loss: {}".format(j, l_epoch))
+                if epoch % log == 0:
+                    print("Epoch: {}, Loss: {}".format(epoch, l_epoch))
 
         self.model = w
         self.losses = loss_list
 
     def predict(self, x):
-        x = np.hstack((np.ones((x.shape[0], 1)), x))
+        if self.bias:
+            x = np.hstack((np.ones((x.shape[0], 1)), x))
+
         p = self.sigmoid(x @ self.model)
         mask_true = p >= 0.5
         mask_false = p < 0.5
