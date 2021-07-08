de4bc95110bafde8e782227e65659d8e652736d8
Agustin Acerbo <80132586+AguAcerbo@users.noreply.github.com>
2021-07-08 11:58:27 -0300

    cats_and_dogs

--- B3-Computer_Vision_2/Clase_2/debug.py
+++ B3-Computer_Vision_2/Clase_2/debug.py
@@ -4,4 +4,23 @@ Created on Wed Jul  7 11:45:23 2021
 
 @author: Agustin
 """
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D , MaxPool2D , Flatten , Dropout 
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+
+from sklearn.metrics import classification_report,confusion_matrix
+
+import tensorflow as tf
+
+import cv2
+import os
+
+import numpy as np
+
+
+cats_train = tf.keras.preprocessing.image.load_img(r'Documents\VpC2_Datasets\Clase2\perros_y_gatos\train\cats')
