b58b7dffc856809fada87d4005d5fa8f28b020ad
Agustin Acerbo <80132586+AguAcerbo@users.noreply.github.com>
2021-08-04 20:25:46 -0300

    Actualizacion

--- Agu_Library/kfold_pipeline_v1.py
+++ Agu_Library/kfold_pipeline_v1.py
@@ -10,21 +10,22 @@ from sklearn.model_selection import KFold
 from sklearn.base import clone
 from sklearn.metrics import roc_auc_score
 
-def kfold_pipeline(models_pipeline, samples , target, k = 5):
+def kfold_pipeline(models_pipeline, samples , target, x_test, y_test, k = 5):
     
     """
     Esta funcion recibe:
         models_pipeline: el pipeline de los modelos a evaluar
         samples: set de entrenamiento
         target: salidas del set de entrenamiento
+        x_test: set de testeo
+        y_test: set de testeo
         k: el numero de segmentos que utilizara el algoritmo de K-Fold
     """
     results = {}
-    for m in models_pipeline:
-        model_name = m[0]
+    for model in models_pipeline:
+        model_name = model[0]
         results[model_name] = {}
         
-        # ----train_auc, valid_auc = EvaluateKFolds(m, x_train, y_train, k)
         train_auc = 0
         valid_auc = 0
         
@@ -37,9 +38,9 @@ def kfold_pipeline(models_pipeline, samples , target, k = 5):
             x_train = samples.iloc[train_index]
             y_train = target.iloc[train_index]
             
-            m[1].fit(x_train, y_train)
-            pred_train = m[1].predict_proba(x_train)
-            pred_valid = m[1].predict_proba(x_valid)
+            model[1].fit(x_train, y_train)
+            pred_train = model[1].predict_proba(x_train)
+            pred_valid = model[1].predict_proba(x_valid)
             
             if len(pred_train.shape)>1 and (pred_train.shape[1]>1):
                 train_auc = train_auc + roc_auc_score(y_train, pred_train[:,1]) / k
@@ -48,8 +49,11 @@ def kfold_pipeline(models_pipeline, samples , target, k = 5):
                 train_auc = train_auc + roc_auc_score(y_train, pred_train) / k
                 valid_auc = valid_auc + roc_auc_score(y_valid, pred_valid) / k
         
+        pred_test = model[1].predict_proba(x_test)
+
         results[model_name]["Train"] = train_auc
         results[model_name]["Valid"] = valid_auc
-        
+        results[model_name]["test"] = roc_auc_score(y_test, pred_test[:,1])
+
     models_results = pd.DataFrame(results).T
     return models_results
